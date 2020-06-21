9ad1c22e4b31edf76e7c52286937b1e4c86cfe24
HyunHoMoon <44826442+gusgh9176@users.noreply.github.com>
2020-06-21 22:51:35 +0900

    추가 업로드 시 폴더 생성하여 저장

--- src/main/java/com/server/transfer_app_server/web/WebRestController.java
+++ src/main/java/com/server/transfer_app_server/web/WebRestController.java
@@ -2,7 +2,6 @@ package com.server.transfer_app_server.web;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.server.transfer_app_server.dto.MobileTokenMainReponseDto;
 import com.server.transfer_app_server.dto.MobileTokenNameResponseDto;
 import com.server.transfer_app_server.dto.MobileTokenReadReqeustDto;
 import com.server.transfer_app_server.dto.MobileTokenSaveRequestDto;
@@ -18,8 +17,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.List;
 
 
@@ -32,10 +29,21 @@ public class WebRestController {
 
     @PostMapping(value = "/upload")
     @ResponseBody
-    public void fileUpload(@RequestPart MultipartFile files) {
+    public void fileUpload(HttpServletRequest request, @RequestPart MultipartFile files) {
+        String userDir = request.getHeader("User-Agent");
+        System.out.println("userDir: " + userDir);
+        String baseDir = "C:\\ServerFiles\\" + userDir;
+        File userFolder = new File(baseDir); // user 폴더
         try {
-            String baseDir = "C:\\ServerFiles";
-            files.transferTo(new File(baseDir + "\\" + files.getOriginalFilename()));
+            // 유저 폴더 없을 시 생성
+            if(!userFolder.exists()){
+                userFolder.mkdir();
+                System.out.println(baseDir + "의 폴더가 생성되었습니다.");
+                System.out.println("경로: " + userFolder);
+            }
+            File dir = new File(baseDir + "\\" + files.getOriginalFilename());
+
+            files.transferTo(dir);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -53,23 +61,24 @@ public class WebRestController {
         String senderToken = mobileTokenService.findByName(senderName).getToken(); // 송신자 token
         String receiverToken = vo.getToken(); // 수신자 Token
 
+        int senderTokenLength = senderToken.length();
+
         boolean isExist = mobileTokenService.isExistToken(receiverToken);
         // 수신자 token 이 서버 DB에 저장 안되있으면 이상한 접근이므로 다운로드 실행 안함
-        if(!isExist){
+        if (!isExist) {
             System.out.println("DB에 수신자 token 미존재");
             System.out.println("앱 외에서의 접근");
             return;
         }
         // 송신자 token 이 서버 DB에 저장 안되있으면 이상한 접근이므로 다운로드 실행 안함
-        else if(senderToken.equals("wrongName")){
+        else if (senderToken.equals("wrongName")) {
             System.out.println("DB에 송신자 token 미존재");
             return;
         }
 
         File[] fileList = null;
         try {
-//            String baseDir = "C:\\ServerFiles\\" + senderName; // 송신자 name 폴더로 들어감
-            String baseDir = "C:\\ServerFiles\\"; // 송신자 name 폴더로 들어감
+            String baseDir = "C:\\ServerFiles\\" + senderToken.substring(senderTokenLength-10, senderTokenLength); // 송신자 name 폴더로 들어감
             fileList = new File(baseDir).listFiles();
         } catch (Exception e) {
             e.printStackTrace();
@@ -116,7 +125,7 @@ public class WebRestController {
         boolean isExist = mobileTokenService.isExistToken(token);
 
         // 보낸쪽 token 서버 DB에 저장 안되있으면 이상한 접근이므로 유저 목록 제공 안함
-        if(!isExist){
+        if (!isExist) {
             System.out.println("미존재");
             System.out.println("앱 외에서의 접근");
             return;
@@ -135,7 +144,7 @@ public class WebRestController {
             response.setHeader("Pragma", "no-cache;");
             response.setHeader("Expires", "-1;");
             response.getWriter().write(jsonStr);
-        }catch (IOException ioe){
+        } catch (IOException ioe) {
             ioe.printStackTrace();
 //            throw new RuntimeException("Json Parsing Error");
         }
@@ -143,21 +152,21 @@ public class WebRestController {
 
     // DB에 Name과 FCMToken 저장
     @PostMapping(value = "mobile/insert/FCMToken")
-    public Long saveFCMToken(@RequestBody MobileTokenSaveRequestDto dto) throws Exception{
-        System.out.println("name: "+dto.getName());
-        System.out.println("token: "+dto.getToken());
+    public Long saveFCMToken(@RequestBody MobileTokenSaveRequestDto dto) throws Exception {
+        System.out.println("name: " + dto.getName());
+        System.out.println("token: " + dto.getToken());
         return mobileTokenService.save(dto);
     }
 
     // DB에 Name 업데이트
     @RequestMapping(value = "mobile/update/FCMToken")
-    public Long updateName(@RequestBody MobileTokenSaveRequestDto dto) throws Exception{
+    public Long updateName(@RequestBody MobileTokenSaveRequestDto dto) throws Exception {
         return mobileTokenService.update(dto);
     }
 
     // DB 목록 확인
     @RequestMapping(value = "mobile/print/FCMToken")
-    public void printName() throws Exception{
+    public void printName() throws Exception {
         System.out.println("현재 토큰 목록");
         mobileTokenService.findAllDescPrint();
         System.out.println("현재 토큰 목록 끝");
@@ -166,15 +175,16 @@ public class WebRestController {
     // Name으로 받아서 Token 찾아야함
     // vo는 보낼 사람의 name
     @PostMapping(value = "mobile/send/FCMToken")
-    public String sendFCMMessage(Model model, HttpServletRequest request, HttpSession session, @RequestBody MobileTokenVO vo) throws Exception {
+    public void sendFCMMessage(Model model, HttpServletRequest request, HttpSession session, @RequestBody MobileTokenVO vo) throws Exception {
 
         String receiverToken = mobileTokenService.findByName(vo.getName()).getToken(); // 수신자 token
         String senderName = mobileTokenService.findByToken(vo.getToken()).getName(); // 송신자 name
 
-        System.out.println("token: "+receiverToken);
-        System.out.println("name: "+senderName);
+        if(receiverToken.equals("wrongName") || senderName.equals("wrongToken")){
+            System.out.println("DB에 없는 name 또는 token");
+            return;
+        }
         fcmService.sendDownloadFCMMessage(senderName, receiverToken);
 
-        return "jsonView";
     }
 }
